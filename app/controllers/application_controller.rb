745c44c49f2d77629e47c5cda91bed4e195f7c7a
naomi <naomi.goldberg@me.com>
2015-07-03 03:17:34 +0000

    added goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world"
+    render text: "hello, world!"
   end
-  
+  def hello_spn
+    render text: "¡Hola, mundo!"
+  end 
+  def goodbye
+    render text: "goodbye, world!"
+  end
 end
